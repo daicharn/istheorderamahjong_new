81b7829d8bbb3e7d054350e1513bdb1762b71ba9
daicharn <fukui-d@outlook.jp>
2026-07-21 17:29:40 +0900

    テストクラスの変更およびblockhaislistのソートの仕様変更

--- tests/toBlock.test.ts
+++ tests/toBlock.test.ts
@@ -1,8 +1,4 @@
 import { Hais } from "../modules/Hais";
-import { PlayerHand } from "../modules/PlayerHand";
-import { Melds } from "../modules/Melds";
-import { PlayerContext } from "../modules/PlayerContext";
-import { YakuChecker } from "../modules/YakuChecker";
 import { BlockHaisList } from "../modules/BlockHaisList";
 import {BlockDivider} from '../modules/BlockDivider';
 
@@ -20,38 +16,51 @@ function getBlockStrings(hais_num: number[]): string[]{
 test("tehai_normal_1", () => {
     const results = getBlockStrings([1,1,1,1,2,2,2,2,3,3,3,3,4,4]);
 
-    expect(results).toContain("[1,1],[1,2,3],[1,2,3],[2,3,4],[2,3,4]");
-    expect(results).toContain("[4,4],[1,1,1],[1,2,3],[2,2,2],[3,3,3]");
-    expect(results).toContain("[4,4],[1,2,3],[1,2,3],[1,2,3],[1,2,3]");
+    const expected: string[] = [
+        "[1,1],[1,2,3],[1,2,3],[2,3,4],[2,3,4]",
+        "[4,4],[1,1,1],[1,2,3],[2,2,2],[3,3,3]",
+        "[4,4],[1,2,3],[1,2,3],[1,2,3],[1,2,3]"
+    ];
+    expect(results).toEqual(expected);
 });
 test("tehai_normal_2", () => {
     const results = getBlockStrings([1,1,1,2,2,2,3,3,3,4,4,4,5,5]);
-    
-    expect(results).toContain("[2,2],[1,1,1],[2,3,4],[3,4,5],[3,4,5]");
-    expect(results).toContain("[5,5],[1,1,1],[2,2,2],[3,3,3],[4,4,4]");
-    expect(results).toContain("[5,5],[1,2,3],[1,2,3],[1,2,3],[4,4,4]");
+
+    const expected: string[] = [
+        "[2,2],[1,1,1],[2,3,4],[3,4,5],[3,4,5]",
+        "[5,5],[1,1,1],[2,2,2],[3,3,3],[4,4,4]",
+        "[5,5],[1,1,1],[2,3,4],[2,3,4],[2,3,4]",
+        "[5,5],[1,2,3],[1,2,3],[1,2,3],[4,4,4]"
+    ];
+    expect(results).toEqual(expected);
 });
 test("tehai_j1m3", () => {
     const results = getBlockStrings([1,1,2,2,2,3,3,3,4,4,4]);
     
-    expect(results).toContain("[1,1],[2,2,2],[3,3,3],[4,4,4]");
-    expect(results).toContain("[1,1],[2,3,4],[2,3,4],[2,3,4]");
-    expect(results).toContain("[4,4],[1,2,3],[1,2,3],[2,3,4]");
+    const expected: string[] = [
+        "[1,1],[2,2,2],[3,3,3],[4,4,4]",
+        "[1,1],[2,3,4],[2,3,4],[2,3,4]",
+        "[4,4],[1,2,3],[1,2,3],[2,3,4]"
+    ];
+    expect(results).toEqual(expected);
 });
 test("tehai_j1m2", () => {
     const results = getBlockStrings([1,1,1,2,2,3,3,3]);
-    
-    expect(results).toContain("[2,2],[1,1,1],[3,3,3]");
+
+    const expected: string[] = ["[2,2],[1,1,1],[3,3,3]"];
+    expect(results).toEqual(expected);
 });
 test("tehai_j1m1", () => {
     const results = getBlockStrings([1,1,1,2,2]);
-    
-    expect(results).toContain("[2,2],[1,1,1]");
+
+    const expected: string[] = ["[2,2],[1,1,1]"];
+    expect(results).toEqual(expected);
 });
 test("tehai_j1m0", () => {
     const results = getBlockStrings([1,1]);
     
-    expect(results).toContain("[1,1]");
+    const expected: string[] = ["[1,1]"];
+    expect(results).toEqual(expected);
 });
 test("isolated_1", () => {
     const results = getBlockStrings([1,2,3,4,5,6,7,8,9,10,12,13,15,15]);
@@ -62,4 +71,16 @@ test("isolated_2", () => {
     const results = getBlockStrings([1,1,2]);
     
     expect(results).toEqual([]);
+});
+test("tehai_sort_1", () => {
+    const results = getBlockStrings([15,15,12,11,10,9,8,7,6,5,4,3,2,1]);
+    
+    const expected: string[] = ["[15,15],[1,2,3],[4,5,6],[7,8,9],[10,11,12]"];
+    expect(results).toEqual(expected);
+});
+test("tehai_sort_2", () => {
+    const results = getBlockStrings([3,9,1,4,11,6,15,8,2,10,5,12,7,15]);
+    
+    const expected: string[] = ["[15,15],[1,2,3],[4,5,6],[7,8,9],[10,11,12]"];
+    expect(results).toEqual(expected);
 });
