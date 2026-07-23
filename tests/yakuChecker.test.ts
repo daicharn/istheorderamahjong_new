3600e669512dfd0b1269c14c32f20e97595da665
daicharn <work@chinokafu.jp>
2026-07-23 11:04:37 +0900

    役判定の追加およびcheckの構造を変更

--- tests/yakuChecker.test.ts
+++ tests/yakuChecker.test.ts
@@ -1,5 +1,4 @@
 import { Hai } from '../modules/Hai';
-import { Hais } from "../modules/Hais";
 import { Melds } from "../modules/Melds";
 import { PlayerHand } from "../modules/PlayerHand";
 import { PlayerContext } from "../modules/PlayerContext";
@@ -7,51 +6,121 @@ import { YakuChecker } from "../modules/YakuChecker";
 import { Meld } from "../modules/Meld";
 import { MeldType } from "../modules/MahjongConsts";
 
-test("tsuiso_1", () => {
-    const hais = new Hais([28,28,29,29,29,31,31,31,32,32,32,34,34,34]);
-    const melds = new Melds();
-    const hand = new PlayerHand([...hais], [...melds]);
+function makeYaku(hais: number[], melds: Meld[] = []): YakuChecker {
+    const hand = new PlayerHand(hais.map(n => new Hai(n)), melds);
     const ctx = new PlayerContext({isTsumo: false, isMenzen: true, playerWind: "E", roundWind: "E"});
-    const yaku = new YakuChecker(hand, ctx);
 
-    const expected = new Map<string, number>();
-    expected.set("字一色", 0);
-    expect(yaku.check()).toEqual(expected);
+    return new YakuChecker(hand, ctx);
+}
+
+describe("字一色", () => {
+    test("tsuiso_1", () => {
+        const yaku = makeYaku([28,28,29,29,29,31,31,31,32,32,32,34,34,34]);
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["字一色", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
+    test("tsuiso_2", () => {
+        const melds = new Melds();
+        melds.add(new Meld([new Hai(32), new Hai(32), new Hai(32)], MeldType.PON));
+        melds.add(new Meld([new Hai(34), new Hai(34), new Hai(34)], MeldType.PON));
+        const yaku = makeYaku([28,28,29,29,29,31,31,31], [...melds])
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["字一色", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
 });
-test("tsuiso_2", () => {
-    const hais = new Hais([28,28,29,29,29,31,31,31]);
-    const melds = new Melds();
-    melds.add(new Meld([new Hai(32), new Hai(32), new Hai(32)], MeldType.PON));
-    melds.add(new Meld([new Hai(34), new Hai(34), new Hai(34)], MeldType.PON));
-    const hand = new PlayerHand([...hais], [...melds]);
-    const ctx = new PlayerContext({isTsumo: false, isMenzen: true, playerWind: "E", roundWind: "E"});
-    const yaku = new YakuChecker(hand, ctx);
+describe("清老頭", () => {
+    test("chinroto_1", () => {
+        const yaku = makeYaku([1,1,1,9,9,9,10,10,18,18,18,27,27,27]);
 
-    const expected = new Map<string, number>();
-    expected.set("字一色", 0);
-    expect(yaku.check()).toEqual(expected);
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["清老頭", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
+    test("chinroto_2", () => {
+        const melds = new Melds();
+        melds.add(new Meld([new Hai(1), new Hai(1), new Hai(1)], MeldType.PON));
+        melds.add(new Meld([new Hai(19), new Hai(19), new Hai(19)], MeldType.PON));
+        const yaku = makeYaku([9,9,9,10,10,18,18,18], [...melds])
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["清老頭", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
 });
-test("chinroto_1", () => {
-    const hais = new Hais([1,1,1,9,9,9,10,10,18,18,18,27,27,27]);
-    const melds = new Melds();
-    const hand = new PlayerHand([...hais], [...melds]);
-    const ctx = new PlayerContext({isTsumo: false, isMenzen: true, playerWind: "E", roundWind: "E"});
-    const yaku = new YakuChecker(hand, ctx);
+describe("緑一色", () => {
+    test("ryuiso_1", () => {
+        const yaku = makeYaku([20,20,21,21,22,22,24,24,24,26,26,26,33,33]);
 
-    const expected = new Map<string, number>();
-    expected.set("清老頭", 0);
-    expect(yaku.check()).toEqual(expected);
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["緑一色", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
+    test("ryuiso_2", () => {
+        const melds = new Melds();
+        melds.add(new Meld([new Hai(20), new Hai(21), new Hai(22)], MeldType.CHI));
+        melds.add(new Meld([new Hai(33), new Hai(33), new Hai(33)], MeldType.PON));
+        const yaku = makeYaku([20,21,22,24,24,24,26,26], [...melds])
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["緑一色", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
 });
-test("chinroto_2", () => {
-    const hais = new Hais([9,9,9,10,10,18,18,18]);
-    const melds = new Melds();
-    melds.add(new Meld([new Hai(1), new Hai(1), new Hai(1)], MeldType.PON));
-    melds.add(new Meld([new Hai(19), new Hai(19), new Hai(19)], MeldType.PON));
-    const hand = new PlayerHand([...hais], [...melds]);
-    const ctx = new PlayerContext({isTsumo: false, isMenzen: true, playerWind: "E", roundWind: "E"});
-    const yaku = new YakuChecker(hand, ctx);
+describe("大四喜", () => {
+    test("daisushi_1", () => {
+        const yaku = makeYaku([1,1,28,28,28,29,29,29,30,30,30,31,31,31]);
 
-    const expected = new Map<string, number>();
-    expected.set("清老頭", 0);
-    expect(yaku.check()).toEqual(expected);
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["大四喜", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
+    test("daisushi_2", () => {
+        const yaku = makeYaku([28,28,28,29,29,29,30,30,30,31,31,31,33,33]);
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["字一色", 0],["大四喜", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
+    test("daisushi_2", () => {
+        const melds = new Melds();
+        melds.add(new Meld([new Hai(29), new Hai(29), new Hai(29)], MeldType.PON));
+        melds.add(new Meld([new Hai(31), new Hai(31), new Hai(31), new Hai(31)], MeldType.ANKAN));
+        const yaku = makeYaku([26,26,28,28,28,30,30,30,], [...melds])
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["大四喜", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
+});
+describe("小四喜", () => {
+    test("shosushi_1", () => {
+        const yaku = makeYaku([1,1,1,28,28,28,29,29,30,30,30,31,31,31]);
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["小四喜", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
+    test("shosushi_2", () => {
+        const yaku = makeYaku([28,28,28,29,29,29,30,30,31,31,31,33,33,33]);
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["字一色", 0],["小四喜", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
+    test("shosushi_3", () => {
+        const melds = new Melds();
+        melds.add(new Meld([new Hai(29), new Hai(29), new Hai(29)], MeldType.PON));
+        melds.add(new Meld([new Hai(31), new Hai(31), new Hai(31)], MeldType.PON));
+        const yaku = makeYaku([25,26,27,28,28,30,30,30], [...melds])
+
+        const expected = new Map<number, Map<string, number>>();
+        expected.set(0, new Map<string, number>([["小四喜", 0]]));
+        expect(yaku.check()).toEqual(expected);
+    });
 });
