25b049fa5ff491d9cdca0476df77cad1470a7ebe
daicharn <work@chinokafu.jp>
2026-07-24 16:21:07 +0900

    待ち牌のテストケースの追加及び整理

--- tests/machi.test.ts
+++ tests/machi.test.ts
@@ -1,66 +1,31 @@
 import { Hais } from '../modules/Hais';
 import { MachiCalculator } from '../modules/MachiCalculator';
+import { casesChinitsu } from './machi/chinitsu';
+import { casesChitoitsu } from './machi/chitoitsu';
+
+type MachiCase = {
+    name: string,
+    hais: number[],
+    expected: number[]
+};
 
 function getMachi(hais_num: number[]): number[] {
     return new MachiCalculator(new Hais(hais_num).getHais()).calculate();
-}
+};
 //4枚使いの待ちを削除する
 function removeFour(hais: number[], results: number[]): number[]{ 
     return results.filter(n => hais.filter(hn => hn === n).length !== 4);
-}
+};
 
-test("machi_9men", () => {
-    const hais = [1,1,1,2,3,4,5,6,7,8,9,9,9];
-    const results = removeFour(hais, getMachi(hais));
+const testcases: Map<string, MachiCase[]> = new Map();
+testcases.set("清一色", casesChinitsu);
+testcases.set("七対子", casesChitoitsu);
 
-    const expected = [1,2,3,4,5,6,7,8,9];
-    expect(results).toEqual(expected);
-});
-test("machi_8men_1", () => {
-    const hais = [1,1,1,2,2,2,3,4,5,6,7,7,7];
-    const results = removeFour(hais, getMachi(hais));
-
-    const expected = [1,2,3,4,5,6,7,8];
-    expect(results).toEqual(expected);
-});
-test("machi_8men_2", () => {
-    const hais = [1,1,1,2,3,4,5,6,6,6,6,7,8];
-    const results = removeFour(hais, getMachi(hais));
-
-    const expected = [1,2,3,4,5,7,8,9];
-
-    expect(results).toEqual(expected);
-});
-test("machi_8men_3", () => {
-    const hais = [1,1,1,3,3,3,4,5,6,7,8,8,8];
-    const results = removeFour(hais, getMachi(hais));
-
-    const expected = [2,3,4,5,6,7,8,9];
-
-    expect(results).toEqual(expected);
-});
-test("machi_7men_1", () => {
-    const hais = [1,1,2,2,2,3,3,3,4,5,6,6,6];
-    const results = removeFour(hais, getMachi(hais));
-
-    const expected = [1,2,3,4,5,6,7];
-
-    expect(results).toEqual(expected);
-});
-test("machi_7men_2", () => {
-    const hais = [2,2,2,3,4,5,6,6,6,7,7,8,8];
-    const results = removeFour(hais, getMachi(hais));
-
-    const expected = [1,3,4,6,7,8,9];
-
-    expect(results).toEqual(expected);
-});
-test("machi_7men_3", () => {
-    const hais = [2,3,3,4,4,5,5,5,5,6,7,7,7];
-    const results = removeFour(hais, getMachi(hais));
-
-    const expected = [1,2,3,4,6,7,8];
-
-    expect(results).toEqual(expected);
-});
-
+testcases.forEach((value, key) => {
+    describe(key, () => {
+        test.each(value)('$name', ({hais, expected}) => {
+            const results = removeFour(hais, getMachi(hais));
+            expect(results).toEqual(expected);
+        });
+    });
+})
