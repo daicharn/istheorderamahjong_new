f60fab3dd114e23e6c6d6b39d9f018a0c1deefef
daicharn <fukui-d@outlook.jp>
2026-07-24 16:21:07 +0900

    待ち牌のテストケースの追加及び整理

---
+++ tests/machi/chinitsu.ts
@@ -0,0 +1,137 @@
+export const casesChinitsu = [
+  {
+    name: "machi_9men",
+    hais: [1,1,1,2,3,4,5,6,7,8,9,9,9],
+    expected: [1,2,3,4,5,6,7,8,9]
+  },
+  {
+    name: "machi_8men_1",
+    hais: [1,1,1,2,2,2,3,4,5,6,7,7,7],
+    expected: [1,2,3,4,5,6,7,8]
+  },
+  {
+    name: "machi_8men_2",
+    hais: [1,1,1,2,3,4,5,6,6,6,6,7,8],
+    expected: [1,2,3,4,5,7,8,9]
+  },
+  {
+    name: "machi_8men_3",
+    hais: [1,1,1,3,3,3,4,5,6,7,8,8,8],
+    expected: [2,3,4,5,6,7,8,9]
+  },
+  {
+    name: "machi_7men_1",
+    hais: [1,1,2,2,2,3,3,3,4,5,6,6,6],
+    expected: [1,2,3,4,5,6,7]
+  },
+  {
+    name: "machi_7men_2",
+    hais: [2,2,2,3,4,5,6,6,6,7,7,8,8],
+    expected: [1,3,4,6,7,8,9]
+  },
+  {
+    name: "machi_7men_3",
+    hais: [2,3,3,4,4,5,5,5,5,6,7,7,7],
+    expected: [1,2,3,4,6,7,8]
+  },
+  {
+    name: "machi_7men_4",
+    hais: [2,2,3,3,4,4,4,5,6,6,6,6,7],
+    expected: [1,2,3,4,5,7,8]
+  },
+  {
+    name: "machi_6men_1",
+    hais: [3,3,4,4,5,5,5,6,7,8,9,9,9],
+    expected: [2,3,4,5,7,8]
+  },
+  {
+    name: "machi_6men_2",
+    hais: [2,2,2,3,3,3,4,4,4,5,5,5,6],
+    expected: [1,3,4,5,6,7]
+  },
+  {
+    name: "machi_6men_3",
+    hais: [2,2,2,3,3,3,4,4,4,4,5,6,7],
+    expected: [1,2,3,5,7,8]
+  },
+  {
+    name: "machi_5men_1",
+    hais: [2,3,4,4,4,5,5,6,7,8,9,9,9],
+    expected: [1,3,4,5,6]
+  },
+  {
+    name: "machi_5men_2",
+    hais: [1,1,1,2,3,3,4,5,5,5,6,7,8],
+    expected: [1,2,3,4,5]
+  },
+  {
+    name: "machi_5men_3",
+    hais: [3,4,5,5,6,6,7,7,8,8,8,8,9],
+    expected: [3,4,6,7,9]
+  },
+  {
+    name: "machi_4men_1",
+    hais: [1,1,1,3,4,5,7,7,7,8,9,9,9],
+    expected: [6,7,8,9]
+  },
+  {
+    name: "machi_4men_2",
+    hais: [2,3,4,4,4,5,5,5,6,6,6,9,9],
+    expected: [1,4,7,9]
+  },
+  {
+    name: "machi_4men_3",
+    hais: [2,2,3,4,4,4,5,5,5,5,6,6,6],
+    expected: [2,3,4,7]
+  },
+  {
+    name: "machi_3men_1",
+    hais: [1,1,1,2,2,3,4,4,5,6,6,7,8],
+    expected: [2,3,5]
+  },
+  {
+    name: "machi_3men_2",
+    hais: [1,1,2,2,2,3,3,4,5,6,7,7,7],
+    expected: [1,2,3]
+  },
+  {
+    name: "machi_3men_3",
+    hais: [1,2,3,3,3,4,5,6,7,8,9,9,9],
+    expected: [3,6,9]
+  },
+  {
+    name: "machi_2men_1",
+    hais: [1,2,2,2,3,3,4,5,5,6,7,8,9],
+    expected: [4,7]
+  },
+  {
+    name: "machi_2men_2",
+    hais: [2,2,2,3,3,4,4,4,6,7,7,7,8],
+    expected: [3,7]
+  },
+  {
+    name: "machi_2men_3",
+    hais: [4,4,4,4,5,5,5,5,7,7,8,8,8],
+    expected: [3,6]
+  },
+  {
+    name: "machi_1men_1",
+    hais: [2,2,3,4,4,4,5,5,5,6,6,8,9],
+    expected: [7]
+  },
+  {
+    name: "machi_1men_2",
+    hais: [1,2,2,3,4,4,5,6,7,7,7,8,9],
+    expected: [3]
+  },
+  {
+    name: "machi_1men_3",
+    hais: [3,3,4,4,4,4,5,5,5,6,7,7,9],
+    expected: [8]
+  },
+  {
+    name: "machi_1men_4",
+    hais: [1,2,2,2,2,3,3,4,5,5,7,8,9],
+    expected: [5]
+  },
+];
---
+++ tests/machi/chitoitsu.ts
@@ -0,0 +1,12 @@
+export const casesChitoitsu = [
+  {
+    name: "machi_chitoi_1",
+    hais: [1,1,3,3,5,5,12,12,14,14,22,22,31],
+    expected: [31]
+  },
+  {
+    name: "machi_chitoi_2",
+    hais: [1,1,2,2,3,3,4,4,5,5,6,6,7],
+    expected: [1,4,7]
+  },
+];
