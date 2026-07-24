c27c3b237f43f509343cc3f7face3508ab75de6a
daicharn <fukui-d@outlook.jp>
2026-07-24 17:23:02 +0900

    blockdividerをリファクタリング（途中まで、テストはレッドの状態）

--- modules/BlockDivider.ts
+++ modules/BlockDivider.ts
@@ -1,7 +1,7 @@
 import { Hai } from './Hai';
 import { BlockHais } from './BlockHais';
 import { BlockHaisList } from './BlockHaisList';
-import { BlockType } from './MahjongConsts';
+import { BlockType, PAI_TYPE_NUM } from './MahjongConsts';
 
 export class BlockDivider{
     private readonly hais: Hai[];
@@ -31,64 +31,59 @@ export class BlockDivider{
     divide(): BlockHaisList[]{
         const results: BlockHaisList[] = [];
         const blockhaislist: BlockHaisList = new BlockHaisList();
+        const counts: number[] = new Array(PAI_TYPE_NUM).fill(0);
+        this.hais.forEach(h => counts[h.getId() - 1]++);
 
         if(this.hais.length > 14) return results;
 
-        const dfs = (arr: Hai[], blocks: BlockHaisList) => {
-            if(arr.length === 0 && blocks.isStandardHand(Math.floor(this.hais.length / 3))){
+        const dfs = (arr: number[], blocks: BlockHaisList) => {
+            if(arr.every(n => n === 0) && blocks.isStandardHand(Math.floor(this.hais.length / 3))){
                 results.push(blocks.clone());
                 return;
             }
 
-            for(let j = 0; j < arr.length; j++){
-                const first = arr[j];
+            for(let j = 0; j < counts.length; j++){
+                const first = counts[j];
+                const firsthai = new Hai(first);
 
                 //刻子の処理
-                if(arr.filter(h => h.getId() === first.getId()).length >= 3){
-                    const next = arr.slice();
-                    this.removeHai(next, first);
-                    this.removeHai(next, first);
-                    this.removeHai(next, first);
+                if(first >= 3){
+                    const next = [...arr];
+                    next[j] -= 3;
 
-                    blocks.push(new BlockHais(BlockType.KOTSU, [first.clone(), first.clone(), first.clone()]));
+                    blocks.push(new BlockHais(BlockType.KOTSU, [firsthai, firsthai, firsthai]));
                     dfs(next, blocks);
                     blocks.pop();
                 }
                 //順子の処理
-                if(first.isNumberTile() && first.num <= 7){
-                    const id1 = first.getId() + 1;
-                    const id2 = first.getId() + 2;
+                if(counts[j] > 0 && counts[j + 1] > 0 && counts[j + 2] > 0 && firsthai.isNumberTile() && firsthai.num <= 7){
+                    const h1 = new Hai(first + 1);
+                    const h2 = new Hai(first + 2);
 
-                    const h1 = arr.find(h => h.getId() === id1);
-                    const h2 = arr.find(h => h.getId() === id2);
+                    const next = [...arr];
+                    next[j]--;
+                    next[j + 1]--;
+                    next[j + 2]--;
 
-                    if(h1 && h2){
-                        const next = arr.slice();
-                        this.removeHai(next, first);
-                        this.removeHai(next, h1);
-                        this.removeHai(next, h2);
-
-                        blocks.push(new BlockHais(BlockType.SHUNTSU, [first.clone(), h1.clone(), h2.clone()]));
-                        dfs(next, blocks);
-                        blocks.pop();
-                    }
+                    blocks.push(new BlockHais(BlockType.SHUNTSU, [firsthai, h1, h2]));
+                    dfs(next, blocks);
+                    blocks.pop();
                 }
             }
         };
 
         //通常の面子の組み合わせの判定
         for(let i = 0; i < this.hais.length; i++){
-            const first = this.hais[i];
+            const firstid = this.hais[i].getId();
 
             if(i > 0 && this.hais[i].getId() === this.hais[i - 1].getId()) continue;
 
             //雀頭の処理
-            if(this.hais.filter(h => h.getId() === first.getId()).length >= 2){
-                const next = this.hais.slice();
-                this.removeHai(next, first);
-                this.removeHai(next, first);
+            if(this.hais.filter(h => h.getId() === firstid).length >= 2){
+                const next = [...counts];
+                next[firstid - 1]--;
 
-                blockhaislist.push(new BlockHais(BlockType.JANTO, [first.clone(), first.clone()]));
+                blockhaislist.push(new BlockHais(BlockType.JANTO, [this.hais[i].clone(), this.hais[i].clone()]));
                 dfs(next, blockhaislist);
                 blockhaislist.pop();
             }
