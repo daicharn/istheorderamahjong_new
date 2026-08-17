e92620ec2eeeba022deb2726405f247f8d8e88b9
daicharn <work@chinokafu.jp>
2026-08-17 15:09:43 +0900

    プロパティ名の変更

--- modules/yaku/YakuChecker.ts
+++ modules/yaku/YakuChecker.ts
@@ -6,21 +6,21 @@ import { YakuContext } from './YakuContext';
 import { NormalYakuChecker } from './NormalYakuChecker';
 
 export class YakuChecker {
-    private yakuContext: YakuContext
+    private context: YakuContext
 
-    constructor(yakuContext: YakuContext){
-        this.yakuContext = yakuContext;
+    constructor(context: YakuContext){
+        this.context = context;
     }
 
     check(): Map<string, number> {
         //手牌と鳴き（カンを考慮して鳴き一つを3と数える）が計14枚かどうか確認する
-        const tehai_num = this.yakuContext.hais.length;
-        const furo_num = this.yakuContext.melds.length;
+        const tehai_num = this.context.hais.length;
+        const furo_num = this.context.melds.length;
         if(tehai_num + furo_num * 3 !== 14) return new Map<string, number>();
 
-        const yakuman_map = new YakumanChecker(this.yakuContext).check();
+        const yakuman_map = new YakumanChecker(this.context).check();
         if(yakuman_map.size === 0){
-            return new NormalYakuChecker(this.yakuContext).check();
+            return new NormalYakuChecker(this.context).check();
         }
         else{
             return yakuman_map;
