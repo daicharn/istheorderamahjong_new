68df4c393be24d9d0e7d435a53ddf3d208347c37
daicharn <work@chinokafu.jp>
2026-08-19 11:29:15 +0900

    yakuChecker.testをyaku.testに改名

---
+++ tests/yaku.test.ts
@@ -0,0 +1,26 @@
+import { YakuChecker } from "../modules/yaku/YakuChecker";
+import { casesYakuman } from './yaku/yakuman';
+import { casesNormal } from './yaku/normal';
+import { TehaiCase } from './testConsts';
+import { YakuContext } from '../modules/yaku/YakuContext';
+import { TehaiCaseRunner } from './tehaiCaseRunner';
+
+const testcases: TehaiCase[] = [];
+casesYakuman.forEach(caseyakuman => testcases.push(caseyakuman));
+casesNormal.forEach(casenormal => testcases.push(casenormal));
+
+testcases.forEach(testcase => {
+    describe(testcase.desc, () => {
+        test(testcase.name, () => {
+            const yakuMaps: Map<number, Map<string, number>> = new Map<number, Map<string, number>>();
+            const runner: TehaiCaseRunner = new TehaiCaseRunner(testcase);
+            const blocks = runner.blocks;
+            blocks.forEach((block, index) => {
+                const context = new YakuContext(runner.hand, runner.ctx, block);
+                const yakuMap = new YakuChecker(context).check();
+                if(yakuMap.size > 0) yakuMaps.set(index, yakuMap);
+            });
+            expect(yakuMaps).toEqual(testcase.expected);
+        });
+    });
+});
