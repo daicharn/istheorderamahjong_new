0877699788f6228fb8555ac9a0cf5e92a0bfa870
daicharn <work@chinokafu.jp>
2026-08-19 09:48:08 +0900

    haisとmeldsをgetアクセサを使用するように変更

--- modules/yaku/YakuContext.ts
+++ modules/yaku/YakuContext.ts
@@ -6,16 +6,20 @@ import { BlockHaisList } from '../BlockHaisList';
 
 export class YakuContext {
     readonly hand: PlayerHand;
-    readonly hais: Hai[];
     readonly block: BlockHaisList;
-    readonly melds: Meld[];
     readonly ctx: PlayerContext;
 
     constructor(hand: PlayerHand, ctx: PlayerContext, block: BlockHaisList){
         this.hand = hand;
-        this.hais = hand.getTehai();
-        this.melds = hand.getFuro();
         this.ctx = ctx;
         this.block = block;
+    }
+
+    get hais(): Hai[]{
+        return this.hand.getTehai();
+    }
+
+    get melds(): Meld[]{
+        return this.hand.getFuro();
     }
 }
